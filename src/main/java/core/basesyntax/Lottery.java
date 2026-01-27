6a3007eae10e7a4f23e21a1d1bc3428fea6783cd
Ilona Chupa <ilonachupa@gmail.com>
2026-01-27 20:33:37 +0200

    Update Lottery.java

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,9 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
+    private static final int MAX_BALL_NUMBER = 100;
+    private Random random = new Random().nextInt(MAX_BALL_NUMBER) + 1;
+    
     public Ball getRandomBall(){
-        return new Ball(new ColorSupplier().getRandomColor(),
-                new Random().nextInt(100) + 1);;
+        return new Ball(new ColorSupplier().getRandomColor(),random) ;
     }
 
 }
