2e18217983f74f2e0a3f509bf0ec316900540a86
Ilona Chupa <ilonachupa@gmail.com>
2026-01-29 15:46:40 +0200

    Update Application.java

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,9 +2,10 @@ package core.basesyntax;
 
 public class Application {
     private static final int BALLS_COUNT = 3;
+    
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        for (int i = 0; i < ballsCount; i++) {
+        for (int i = 0; i < BALLS_COUNT; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
