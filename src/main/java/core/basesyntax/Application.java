ca92c557f363dccbd8d842c7dcb7fed938c9bec1
Ilona Chupa <ilonachupa@gmail.com>
2026-01-29 15:12:50 +0200

    Update Application.java
    made BALLS_COUNT non final

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         // create three balls using class Lottery and print information about them in console
-        private static final int BALLS_COUNT = 3;
+        static final int BALLS_COUNT = 3;
 
         Lottery lottery = new Lottery();
         for (int i = 0; i < BALLS_COUNT; i++) {
