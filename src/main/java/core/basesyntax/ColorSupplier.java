07f551e457e52367b07c9a4533a84681d09a9200
Ilona Chupa <ilonachupa@gmail.com>
2026-01-29 15:31:58 +0200

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class ColorSupplier {
     private Random random = new Random();
+    
     public Colors getRandomColor() {
         return Colors.values()[random.nextInt(Colors.values().length)];
     }
